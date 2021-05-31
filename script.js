12f0287594c21a1e6864231da0b285bb3b2111b7
Hubert <konczito33@gmail.com>
2021-06-01 00:48:01 +0200

    adding select

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
   //API URLS
   const BASE_URL = 'https://api.themoviedb.org/3/'
   const BY_POPULARITY_URL = 'discover/movie?sort_by=popularity.desc&api_key=8efd5c56d10b8332b9691c3d750ff1bb&page='
+  const BY_RELEASE_DATE = 'discover/movie?sort_by=release_date.desc&api_key=8efd5c56d10b8332b9691c3d750ff1bb&page='
+  const BY_RELEASE_DATE_ASCENDING = 'discover/movie?sort_by=release_date.asc&api_key=8efd5c56d10b8332b9691c3d750ff1bb&page='
+  const BY_AVERAGE_VOTE = 'discover/movie?sort_by=vote_average.desc&api_key=8efd5c56d10b8332b9691c3d750ff1bb&page='
+  const BY_AVERAGE_VOTE_ASCENDING = 'discover/movie?sort_by=vote_average.asc&api_key=8efd5c56d10b8332b9691c3d750ff1bb&page='
   const SEARCH_URL = 'search/movie?api_key=8efd5c56d10b8332b9691c3d750ff1bb&query='
   const IMG_URL = 'https://image.tmdb.org/t/p/w1280/'
 
@@ -14,6 +18,13 @@
   const pageNumEl = document.querySelector('.pages-nav__counter')
   const pageNumContainer = document.querySelector('.pages-nav')
 
+  //testy
+  const dropdownText = document.querySelector('.dropdown__text')
+  const selectInput = document.querySelector('.select')
+  const options = document.querySelectorAll('.dropdown__item')
+  const URLs = [BY_POPULARITY_URL, BY_RELEASE_DATE, BY_RELEASE_DATE_ASCENDING, BY_AVERAGE_VOTE, BY_AVERAGE_VOTE_ASCENDING]
+  let actualURL = BY_POPULARITY_URL
+  let selectCounter = 0;
 
 
   //GET DATA FROM API
@@ -21,12 +32,12 @@
     try {
       const res = await fetch(url)
       const data = await res.json()
+      console.log(data)
       createMovieEl(data.results)
       if (data.results == '') {
         showError()
       }
-    } catch (err) {
-    }
+    } catch (err) {}
   }
 
   //CREATE ERROR ELEMENT
@@ -51,19 +62,23 @@
       const movieHTML = document.createElement('div')
       movieHTML.classList.add('movie')
       movieHTML.innerHTML = `<img class="movie__img" src="${IMG_URL + poster_path}" alt="">
-            <div class="movie__info">
-            <p class="movie__title">${title}</p>
-            <span class="movie__rating ${changeColorOfRating(vote_average)}">${vote_average}</span>
-            </div>
-            <div class="overview">
-            <h3 class="overview__heading">Overview</h3>
-            <p class="overview__text">
-            ${overview}
-            </p>
-            <span class="overview__date">${release_date}</span>
-            </div>`
-
+      <div class="movie__info">
+      <p class="movie__title">${title}</p>
+      <span class="movie__rating ${changeColorOfRating(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+      <h3 class="overview__heading">Overview</h3>
+      <p class="overview__text">
+      ${overview}
+      </p>
+      <span class="overview__date">${release_date}</span>
+      </div>`
       container.appendChild(movieHTML)
+      if (poster_path === null) {
+        movieHTML.style.display = 'none'
+      } else {
+        movieHTML.style.display = 'flex'
+      }
     })
   }
 
@@ -112,9 +127,17 @@
     pageNumEl.textContent = page
   }
 
+
+  function dropdownUpdateText(e) {
+    const target = e.target
+    const item = target.getAttribute('data-name')
+    dropdownText.textContent = item
+  }
+
+
   //BACK TO MAIN PAGE BY CLICKING AT LOGO
   logo.addEventListener('click', () => {
-    getMovieFromApi(BASE_URL + BY_POPULARITY_URL)
+    getMovieFromApi(BASE_URL + URLs[selectCounter])
     page = 1
     updatePageNumber()
     window.scrollTo(0, 0)
@@ -126,7 +149,7 @@
     page--
     checkButton()
     updatePageNumber()
-    getMovieFromApi(BASE_URL + BY_POPULARITY_URL + page)
+    getMovieFromApi(BASE_URL + URLs[selectCounter] + page)
     window.scrollTo(0, 0)
   })
 
@@ -134,13 +157,37 @@
     page++
     checkButton()
     updatePageNumber()
-    getMovieFromApi(BASE_URL + BY_POPULARITY_URL + page)
+    getMovieFromApi(BASE_URL + URLs[selectCounter] + page)
     window.scrollTo(0, 0)
   })
 
-  getMovieFromApi(BASE_URL + BY_POPULARITY_URL)
+  getMovieFromApi(BASE_URL + URLs[selectCounter])
   updatePageNumber()
 
 
 
+  //testy
+
+
+
+
+  options.forEach(option => {
+    option.addEventListener('click', (e) => {
+      selectCounter = e.target.getAttribute('data-select')
+      checkButton()
+      updatePageNumber()
+      getMovieFromApi(BASE_URL + URLs[selectCounter])
+      dropdownUpdateText(e)
+      window.scrollTo(0, 0)
+    })
+  })
+
+  selectInput.addEventListener('change', (e) => {
+    page = 1
+    checkButton()
+    updatePageNumber()
+    selectCounter = e.target.value
+    getMovieFromApi(BASE_URL + URLs[selectCounter])
+    console.log(actualURL)
+  })
 }())
